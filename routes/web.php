22fc8e8cf0dbadb823c39f739f486b73b8756d21
Paisan Aunyaw <ap.jmsjame@gmail.com>
2018-06-16 17:24:17 +0700

    Add template

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,13 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::get('apanel', function () {
+    return view('admin');
+});
+
+Route::get('test', function (){
+    return view('test');
 });
